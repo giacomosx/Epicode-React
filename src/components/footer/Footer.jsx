0b3186163837d657dd9f0c6231fbfe13bdaea862
= <giacomo.bartoli@me.com>
2024-04-15 18:03:54 +0200

    fix footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
     <footer className='py-4 justify-content-center align-items-center d-flex flex-column  bg-body-tertiary '>
-        <p className='m-0'>&copy; {date.getFullYear()} EpiBooks - Created with React by <a href='https://github.com/giacomosx' target='_blank' rel="noreferrer" className=' text-primary '>@giacomosx</a> </p>
+        <p className='m-0 text-center small w-75 '>&copy; {date.getFullYear()} EpiBooks - Created with React by 🥷&nbsp;<a href='https://github.com/giacomosx' target='_blank' rel="noreferrer" className=' text-primary '>@giacomosx</a> </p>
         
     </footer>
   )
