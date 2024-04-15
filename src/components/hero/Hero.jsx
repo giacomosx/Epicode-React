dfdab62d3b0db0c33de44c4110e1d4fba64ede96
= <giacomo.bartoli@me.com>
2024-04-15 17:01:03 +0200

    last fixes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
       </div>
       <div className='col-12 col-md-6 order-first order-md-last '>
           <div className="p-3 d-flex flex-column align-items-center justify-content-center h-100">
-            <h1 className="display-6">Welcome to EpiBook!</h1>
+            <h1 className="display-6">Welcome to EpiBooks!</h1>
             <blockquote className="text-secondary text-center blockquote">Discover, read, love. The books you desire, just a click away.</blockquote>
             <Button>Explore now</Button>
           </div>
--- src/components/navlist/Navlist.jsx
+++ src/components/navlist/Navlist.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
-import './navlist.css'
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "./navlist.css";
+
+const navLinks = [
+  { title: "Fantasy", selector: "#fantasy" },
+  { title: "History", selector: "#history" },
+  { title: "Horror", selector: "#horror" },
+  { title: "Romance", selector: "#romance" },
+  { title: "Sci-fi", selector: "#sci-fi" },
+];
 
 const Navlist = () => {
   return (
     <>
-        <Navbar.Toggle aria-controls="mainNav" />
-        <Navbar.Collapse id="mainNav" className=' flex-grow-0 '>
-            <Nav className="me-auto ">
-            <Nav.Link className="text-bg-light hover--text-underline " href="#home">Home</Nav.Link>
-            <Nav.Link className="text-bg-light hover--text-underline" href="#fantasy">Fantasy</Nav.Link>
-            <Nav.Link className="text-bg-light hover--text-underline" href="#history">History </Nav.Link>
-            <Nav.Link className="text-bg-light hover--text-underline" href="#horror">Horror</Nav.Link>
-            <Nav.Link className="text-bg-light hover--text-underline" href="#romance">Romance</Nav.Link>
-            <Nav.Link className="text-bg-light hover--text-underline" href="#sci-fi">Sci-fi</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
+      <Navbar.Toggle aria-controls="mainNav" />
+      <Navbar.Collapse id="mainNav" className=" flex-grow-0 ">
+        <Nav className="me-auto ">
+          {
+            navLinks.map((item, index) => (
+              <Nav.Link key={index} className="text-bg-light hover--text-underline" href={item.selector}>{item.title}</Nav.Link>
+            ))
+          }
+        </Nav>
+      </Navbar.Collapse>
     </>
-  )
-}
+  );
+};
 
-export default Navlist
+export default Navlist;
