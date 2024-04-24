32301bde4d931a67ffdb18547f068a9c8dddce0c
= <giacomo.bartoli@me.com>
2024-04-24 16:49:36 +0200

    change header and footer colors in dark mode

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 const Footer = () => {
   const date =  new Date();
 
+  const {isDark} = useContext(ThemeContext)
+
   return (
-    <footer className='py-4 justify-content-center align-items-center d-flex flex-column  bg--violet '>
+    <footer className={`py-4 justify-content-center align-items-center d-flex flex-column ${isDark ? 'bg-dark' : 'bg--violet'} `}>
         <p className='m-0 text-center small w-75 text-white'>&copy; {date.getFullYear()} EpiBooks - Created with React by 🥷&nbsp;<a href='https://github.com/giacomosx' target='_blank' rel="noreferrer" className=' text-white '>@giacomosx</a> </p>
         
     </footer>
