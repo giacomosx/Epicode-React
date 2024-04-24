f610720bbee07c33d1eebd515948e1467f9a11b4
= <giacomo.bartoli@me.com>
2024-04-24 21:15:26 +0200

    change primary color

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react'
-import { ThemeContext } from '../../contexts/ThemeContext';
+import React from 'react'
 
 const Footer = () => {
   const date =  new Date();
 
-  const {isDark} = useContext(ThemeContext)
-
   return (
-    <footer className={`py-4 justify-content-center align-items-center d-flex flex-column ${isDark ? 'bg-dark' : 'bg--violet'} `}>
+    <footer className={`py-4 justify-content-center align-items-center d-flex flex-column bg--violet`}>
         <p className='m-0 text-center small w-75 text-white'>&copy; {date.getFullYear()} EpiBooks - Created with React by 🥷&nbsp;<a href='https://github.com/giacomosx' target='_blank' rel="noreferrer" className=' text-white '>@giacomosx</a> </p>
         
     </footer>
