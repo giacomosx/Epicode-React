68ebb1f2139a4c58bd205d379ae06a962c32e8d8
= <giacomo.bartoli@me.com>
2024-05-07 20:33:19 +0200

    migrate to swiper element

--- src/components/hero/swipercomp/SwiperComp.jsx
+++ src/components/hero/swipercomp/SwiperComp.jsx
@@ -1,46 +1,51 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useRef } from "react";
+import { register } from "swiper/element/bundle";
 import HeroCard from "../herocard/HeroCard";
 import "swiper/css";
 import "swiper/css/pagination";
-import './swipercomp.css'
-import { Autoplay } from "swiper/modules";
+import "./swipercomp.css";
 
 const SwiperComp = ({ data }) => {
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    // Register Swiper web component
+    register();
+
+    // Object with parameters
+    const params = {
+      slidesPerView: 3,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 5,
+        },
+        1440: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+      },
+    };
+
+    Object.assign(swiperRef.current, params);
+
+    // initialize swiper
+    swiperRef.current.initialize();
+  }, []);
+
   return (
-    <div className="container">
-      <Swiper
-        direction="horizontal"
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: true,
-        }}
-        grabCursor
-        modules={[Autoplay]}
-        className="mySwiper"
-        breakpoints={{
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-            autoplay: false
-          },
-          1024: { 
-            slidesPerView: 2,
-            spaceBetween: 5,
-          },
-          1440: { 
-            slidesPerView: 3,
-            spaceBetween: 5
-          },
-        }}
-      >
-        {data.map((book, indx) => (
-          <SwiperSlide className="p-2 h-100 " key={'swiper-' + indx}>
-            <HeroCard title={book.title} img={book.img} price={book.price} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <swiper-container init="false" ref={swiperRef}>
+      {data.map((book, indx) => (
+        <swiper-slide className="p-2 h-100 " key={"swiper-" + indx}>
+          <HeroCard title={book.title} img={book.img} price={book.price} />
+        </swiper-slide>
+      ))}
+    </swiper-container>
   );
 };
 
